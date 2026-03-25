b1d2bcbdc2f7e5725baccb915fc18aaebce9a4e6
Zachary Hill <zacharyh728@gmail.com>
2026-03-24 23:52:16 -0400

    modified configParser to handle comment prefixes

--- network_tracker/config.py
+++ network_tracker/config.py
@@ -6,7 +6,7 @@ _DEFAULT_CONFIG = pathlib.Path(__file__).parent.parent / "config.ini"
 
 
 def load(path: str | None = None) -> configparser.ConfigParser:
-    cfg = configparser.ConfigParser()
+    cfg = configparser.ConfigParser(inline_comment_prefixes=(";", "#"))
     cfg.read(str(path or _DEFAULT_CONFIG))
 
     # Env vars override config file values (useful for Docker secrets)
